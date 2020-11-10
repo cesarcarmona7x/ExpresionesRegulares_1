41c84e63f73c8b97469c2b5b85d155e052ad382b
Cesar Carmona <cesrafcata@gmail.com>
2020-11-10 15:16:28 -0700

    Añadidos los archivos del repositorio.

---
+++ src/sample/Controller.java
@@ -0,0 +1,43 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Controller {
+    @FXML TextField tfTexto;
+    @FXML Label lbCumple;
+    @FXML ListView listCasos;
+    @FXML
+    protected void initialize(){
+        listCasos.getItems().addAll("Ejercicio 1","Ejercicio 2","Validar contraseña (1 mayús, 1 minús, 1 número, 1 caracter de /_+-%#)");
+    }
+    public void procesar(ActionEvent event){
+        String texto=tfTexto.getText();
+        String expresion="";
+        switch(listCasos.getSelectionModel().getSelectedIndex()){
+            case 0://Ejercicio 1
+                expresion="^[_][^p]([a-zA-Z0-9]{3,})[q|w]$";
+                break;
+            case 1://Ejercicio 2
+                expresion="^[+]([a-zA-Z]+)";
+                break;
+            case 2://Validar password
+                expresion="^(?=.*[a-z]+)(?=.*[A-Z]+)(?=.*[0-9]+)(?=.*[^\\w]+).*$";
+                break;
+        }
+        Pattern patron= Pattern.compile(expresion);
+        Matcher matcher=patron.matcher(texto);
+        if(matcher.matches()){
+            lbCumple.setText("Sí cumple");
+        }
+        else{
+            lbCumple.setText("No cumple");
+        }
+    }
+}
